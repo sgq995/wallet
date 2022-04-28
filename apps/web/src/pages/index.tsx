3fbfe1aab747bccacffd2506e04ffca97ced826a
Sebastian G. Q <sgq995@gmail.com>
2022-04-27 20:25:44 -0500

    [FEAT] Home layout added

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,35 @@
-import { Button } from "ui";
+import {
+  Box,
+  Card,
+  Divider,
+  Paper,
+  Stack,
+  Typography,
+} from '../components/Material';
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <>
+      <Typography variant="h1">Home</Typography>
+
+      <Paper variant="outlined">
+        <Stack
+          padding={2}
+          spacing={2}
+          direction="row"
+          justifyContent="space-between"
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <Box flexGrow={3}>
+            <Typography variant="body1">Description</Typography>
+          </Box>
+
+          <Stack flexGrow={1} flexDirection="column" alignItems="flex-end">
+            <Typography variant="body2">100.000.000,00</Typography>
+            <Typography variant="body2">2022/04/23</Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+    </>
   );
 }
