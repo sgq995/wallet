eb2bf8a3a50c3191ea548ea03f08acd0fd93b63b
Sebastian G. Q <sgq995@gmail.com>
2022-05-01 21:57:30 -0500

    [REFACTOR] Move infinite scroll

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,139 +1,7 @@
-import {
-  MutableRefObject,
-  PropsWithChildren,
-  Ref,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useState } from 'react';
 
 import EntryPaper from '../components/EntryPaper';
 import { Stack, Typography } from '../components/Material';
-
-function useRefCallback<R>(
-  callback: (ref: MutableRefObject<R>, element) => any
-) {
-  const ref = useRef<R>();
-  const setRef = useCallback((element) => {
-    callback(ref, element);
-    ref.current = element;
-  }, []);
-
-  return setRef;
-}
-
-function useObserver(
-  callback: IntersectionObserverCallback,
-  options: IntersectionObserverInit
-): IntersectionObserver | undefined {
-  const [observer, setObserver] = useState<IntersectionObserver>(
-    new IntersectionObserver(callback, options)
-  );
-
-  useEffect(() => {
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  return observer;
-}
-
-function useObservedRef<T extends HTMLElement = undefined>(
-  callback: IntersectionObserverCallback,
-  options: IntersectionObserverInit
-) {
-  const observer = useObserver(callback, options);
-
-  const setRef = useRefCallback<T>((ref, element) => {
-    if (ref.current) {
-      observer?.unobserve(ref.current);
-    }
-
-    if (element) {
-      observer?.observe(element);
-    }
-  });
-
-  return setRef;
-}
-
-function ElementWrapper({ children }: PropsWithChildren<{}>) {
-  return <>{children}</>;
-}
-
-interface InfiniteScrollProps<D, R extends HTMLElement = undefined> {
-  data: D[];
-  children: (entry: D, ref?: Ref<R>, index?: number) => JSX.Element;
-}
-
-function InfiniteScroll<D, R extends HTMLElement = undefined>({
-  data,
-  children,
-}: InfiniteScrollProps<D, R>) {
-  const root = useRef();
-
-  const [isFirstIntersecting, setIsFirstIntersecting] = useState(false);
-  const [isLastIntersecting, setIsLastIntersecting] = useState(false);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(10);
-
-  const firstRef = useObservedRef(
-    (entries) => {
-      entries.forEach((entry) => {
-        setIsFirstIntersecting(entry.isIntersecting);
-        if (entry.isIntersecting) {
-          // setEnd((end) => Math.min(end - 1, data.length));
-        } else {
-          // setStart((start) => Math.min(start + 1, data.length));
-        }
-      });
-    },
-    { threshold: 0.0 }
-  );
-
-  const lastRef = useObservedRef(
-    (entries) => {
-      entries.forEach((entry) => {
-        setIsLastIntersecting(entry.isIntersecting);
-        if (entry.isIntersecting) {
-          // setEnd((end) => Math.min(end + 1, data.length));
-        } else {
-          // setStart((start) => Math.max(start - 1, 0));
-        }
-      });
-    },
-    { threshold: 1.0 }
-  );
-
-  useLayoutEffect(() => {
-    console.log({ isFirstIntersecting, isLastIntersecting });
-  }, [isFirstIntersecting, isLastIntersecting]);
-
-  const elements = data
-    .map((entry, index) => {
-      if (start <= index && index < end) {
-        return { entry, key: index };
-      }
-    })
-    .filter((result) => result);
-  // .map((entry, index, array) => {
-  //   const ref = undefined;
-  //   return children(entry, ref, index);
-  // });
-
-  return (
-    <Stack spacing={2} ref={root.current}>
-      <span ref={firstRef}></span>
-      {elements.map(({ entry, key }) => (
-        <ElementWrapper key={key}>{children(entry)}</ElementWrapper>
-      ))}
-      <span ref={lastRef}></span>
-    </Stack>
-  );
-}
 
 export default function Home() {
   const [entries, setEntries] = useState(new Array(50).fill({}));
@@ -143,13 +11,9 @@ export default function Home() {
       <Typography variant="h1">Home</Typography>
 
       <Stack spacing={2}>
-        {/* {entries.map((_, index) => (
+        {entries.map((_, index) => (
           <EntryPaper key={index} />
-        ))} */}
-
-        <InfiniteScroll data={entries}>
-          {({}, ref, index) => <EntryPaper ref={ref} key={index} />}
-        </InfiniteScroll>
+        ))}
       </Stack>
     </>
   );
