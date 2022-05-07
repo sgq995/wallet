b46f16c3e2548bea817a32537376b134cd48113e
Sebastian G. Q <sgq995@gmail.com>
2022-05-07 16:21:27 -0500

    [FEAT] Generic endpoint service added

--- apps/web/src/services/api.ts
+++ apps/web/src/services/api.ts
@@ -1,3 +1,5 @@
+import { objectToUrlSearchParams } from '../utils/url-utils';
+
 const BASE_URL = 'http://localhost:5000';
 
 export class RequestError<T = any> extends Error {
@@ -46,7 +48,7 @@ export function requestWithBaseFactory(routeBase: string) {
       return request<T>(
         {
           ...input,
-          url: `${routeBase}/${input.url}`,
+          url: `${routeBase}${input.url}`,
         },
         init
       );
@@ -54,22 +56,136 @@ export function requestWithBaseFactory(routeBase: string) {
   };
 }
 
-export function requestFactory(routeBase: string) {
-  return function queryFunctionFactory<T>(
-    input: RequestInfo,
-    init?: RequestInit
-  ) {
+export class BaseEndpointService {
+  constructor(private basePath: string) {}
+
+  request<T>(input: RequestInfo, init?: RequestInit) {
     if (typeof input === 'string') {
-      return () => request<T>(`${routeBase}${input}`, init);
+      return request<T>(`${this.basePath}${input}`, init);
     } else {
-      return () =>
-        request<T>(
-          {
-            ...input,
-            url: `${routeBase}/${input.url}`,
-          },
-          init
-        );
+      return request<T>(
+        {
+          ...input,
+          url: `${this.basePath}${input.url}`,
+        },
+        init
+      );
     }
-  };
+  }
+}
+
+export enum EndpointServiceMethod {
+  FindAll = 'findAll',
+  AddOne = 'addOne',
+  FindOne = 'findOne',
+  UpdateOne = 'updateOne',
+  RemoveOne = 'removeOne',
+}
+
+interface IEndpointServiceFindAll<TQuery, TReply> {
+  [EndpointServiceMethod.FindAll]: (query?: TQuery) => Promise<TReply>;
+}
+
+interface IEndpointServiceAddOne<TRequest, TReply> {
+  [EndpointServiceMethod.AddOne]: (body: TRequest) => Promise<TReply>;
+}
+interface IEndpointServiceFindOne<TId, TReply> {
+  [EndpointServiceMethod.FindOne]: (id: TId) => Promise<TReply>;
+}
+interface IEndpointServiceUpdateOne<TId, TRequest, TReply> {
+  [EndpointServiceMethod.UpdateOne]: (
+    id: TId,
+    body: TRequest
+  ) => Promise<TReply>;
+}
+interface IEndpointServiceRemoveOne<TId, TRemoveOne> {
+  [EndpointServiceMethod.RemoveOne]: (id: TId) => Promise<TRemoveOne>;
+}
+
+export interface IEndpointServiceRequest {
+  AddOne?: unknown;
+  UpdateOne?: unknown;
+}
+
+export interface IEndpointServiceReply {
+  FindAll?: unknown;
+  AddOne?: unknown;
+  FindOne?: unknown;
+  UpdateOne?: unknown;
+  RemoveOne?: unknown;
+}
+
+export interface IEndpointServiceOptions {
+  Id?: unknown;
+  Query?: unknown;
+  Request?: IEndpointServiceRequest;
+  Reply?: IEndpointServiceReply;
+}
+
+export type TEndpointService<
+  Options extends IEndpointServiceOptions = IEndpointServiceOptions
+> = BaseEndpointService &
+  IEndpointServiceFindAll<Options['Query'], Options['Reply']['FindAll']> &
+  IEndpointServiceAddOne<
+    Options['Request']['AddOne'],
+    Options['Reply']['AddOne']
+  > &
+  IEndpointServiceFindOne<Options['Id'], Options['Reply']['FindOne']> &
+  IEndpointServiceUpdateOne<
+    Options['Id'],
+    Options['Request']['UpdateOne'],
+    Options['Reply']['UpdateOne']
+  > &
+  IEndpointServiceRemoveOne<Options['Id'], Options['Reply']['RemoveOne']>;
+
+function findAll<TQuery, TFindAll>(this: BaseEndpointService, query?: TQuery) {
+  const searchParams = query ? objectToUrlSearchParams(query) : '';
+  const url = query ? `/?${searchParams}` : '/';
+  return this.request<TFindAll>(url);
+}
+
+function addOne<TRequestAddOne, TReplyAddOne>(
+  this: BaseEndpointService,
+  body: TRequestAddOne
+) {
+  return this.request<TReplyAddOne>('/', {
+    method: 'POST',
+    body: JSON.stringify(body),
+  });
+}
+
+function findOne<TId, TFindOne>(this: BaseEndpointService, id: TId) {
+  return this.request<TFindOne>(`/${id}`);
+}
+
+function updateOne<TId, TRequestUpdateOne, TReplyUpdateOne>(
+  this: BaseEndpointService,
+  id: TId,
+  body: TRequestUpdateOne
+) {
+  return this.request<TReplyUpdateOne>(`/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(body),
+  });
+}
+
+function removeOne<TId, TRemoveOne>(this: BaseEndpointService, id: TId) {
+  return this.request<TRemoveOne>(`/${id}`, {
+    method: 'DELETE',
+  });
+}
+
+type Flatten<T> = T extends any[] ? T[number] : T;
+
+type TEndpointServiceKeys = (keyof TEndpointService)[];
+
+export function endpointService<
+  Options extends IEndpointServiceOptions,
+  Methods extends TEndpointServiceKeys = TEndpointServiceKeys,
+  Keys extends keyof TEndpointService = Flatten<Methods>,
+  Return extends BaseEndpointService = BaseEndpointService &
+    Pick<TEndpointService<Options>, Keys>
+>(basePath: string, methods: Methods): Return {
+  const base: unknown = new BaseEndpointService(basePath);
+  return base as Return;
 }
