0f3d5bba4e392ce73c86ff80924fb21db2a07d78
Sebastian G. Q <sgq995@gmail.com>
2022-05-12 20:22:10 -0500

    [FEAT] Auth was added with SuperTokens

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,9 +3,17 @@ import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 
+import SuperTokensReact from 'supertokens-auth-react';
+
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
+import { frontendConfig } from '../../config/frontendConfig';
+
 const Layout = dynamic(() => import('../components/Layout'));
+
+if (typeof window !== 'undefined') {
+  SuperTokensReact.init(frontendConfig());
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
