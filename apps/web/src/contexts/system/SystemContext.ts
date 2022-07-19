46b387555343cc27e0748f436eaffd2a96e18a5c
Sebastian G. Q <sgq995@gmail.com>
2022-07-18 20:59:32 -0500

    [FEAT] State management for system context was created

--- apps/web/src/contexts/system/SystemContext.ts
+++ apps/web/src/contexts/system/SystemContext.ts
@@ -1,12 +1,15 @@
 import { createContext } from 'react';
+import { TArrayOfCurrencyModel } from 'schemas/currencies/model';
 import { TArrayOfEntryTypeModel } from 'schemas/entry-types/model';
 
 export interface ISystemContext {
   entryTypes: TArrayOfEntryTypeModel;
+  currencies: TArrayOfCurrencyModel;
 }
 
 export const defaultSystemContext: ISystemContext = {
   entryTypes: [],
+  currencies: [],
 };
 
 export const SystemContext =
