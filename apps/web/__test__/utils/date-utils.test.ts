0b524080bf2c6b5bce8fe917579d405bf41667a2
Sebastian G. Q <sgq995@gmail.com>
2022-08-06 13:23:24 -0500

    [DEV] Testing libraries added

---
+++ apps/web/__test__/utils/date-utils.test.ts
@@ -0,0 +1,17 @@
+import { yyyyMMdd } from '../../src/utils/date-utils';
+
+describe('date-utils', () => {
+  it('should return formatted date as string', () => {
+    const date = new Date(2022, 0, 1);
+
+    const result = yyyyMMdd(date);
+    expect(result).toBe('2022/01/01');
+  });
+
+  it('should return formatted date string as string', () => {
+    const date = '2022-01-01';
+
+    const result = yyyyMMdd(date);
+    expect(result).toBe('2022/01/01');
+  });
+});
