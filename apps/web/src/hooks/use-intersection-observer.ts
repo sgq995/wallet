1603131dd02b26c7341d2079f40da6dd51d3e8bb
Sebastian G. Q <sgq995@gmail.com>
2022-10-11 20:32:51 -0500

    [FEAT] IntersectionObserver hook was added

---
+++ apps/web/src/hooks/use-intersection-observer.ts
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+export function useIntersectionObserver(
+  options?: IntersectionObserverInit
+): [(node: any) => void, boolean] {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const lastIntersectionNode = useRef(null);
+  const intersectionObserverRef = useRef<IntersectionObserver>(
+    new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, options)
+  );
+
+  const ref = useCallback((node) => {
+    if (lastIntersectionNode.current) {
+      intersectionObserverRef.current.unobserve(lastIntersectionNode.current);
+      lastIntersectionNode.current = null;
+    }
+
+    if (node === null) {
+      return;
+    }
+
+    lastIntersectionNode.current = node;
+    intersectionObserverRef.current.observe(node);
+  }, []);
+
+  return [ref, isIntersecting];
+}
